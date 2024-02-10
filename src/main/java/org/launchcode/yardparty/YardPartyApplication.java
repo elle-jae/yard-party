6fe9081ab8349f6f4f85cb159c4d95531d2efe07
e <ellen.jardon@gmail.com>
2024-02-09 18:14:07 -0600

    inital commit

---
+++ src/main/java/org/launchcode/yardparty/YardPartyApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.yardparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YardPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YardPartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/yardparty/YardPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.yardparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YardPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
